e1677521d46ef9e7f6f35147208a7ad8b722723d
bigdataleeky <98160533+bigdataleeky@users.noreply.github.com>
2022-02-10 20:58:07 +0900

    Add files via upload

--- 20220209/20220209_8.py
+++ 20220209/20220209_8.py
@@ -1,7 +1,16 @@
-n = 1260
+n = original = 1260
 count = 0
 # 화폐단위
 coin_types = [500,100,50,10]
 # 주어진 화폐단위로 거슬러 줄수 있는 동전의 개수
+for coin in coin_types:
+    count += + n // coin
+    n %= coin
+print(f"{original} 동전 총 갯수 : {count}")
 
+# 500 으로 나누기
+print(1260 // 500)
+print((1260%500) // 100)
+print( ((1260%500)%100) // 50 )
+print( (((1260%500)%100)%50) // 10 )
 
