604407d44ac57120153778bbfb770f258f0f230a
bigdataleeky <98160533+bigdataleeky@users.noreply.github.com>
2022-02-10 21:47:41 +0900

    Add files via upload

--- 20220210/20220210_4.py
+++ 20220210/20220210_4.py
@@ -17,6 +17,7 @@ class Students:
     def inputData(self):  # 데이터 입력
         self.inputdata = input("이름과 국영수를 공백을 기준으로 입력 : ").split()
         self.processScore()
+        return self
 
     def processScore(self):
         self.name = self.inputdata[0]
@@ -29,9 +30,10 @@ class Students:
             except Exception as e:
                 print(e)
         self.avg = self.total / 3
+        # return [self.name,self.kor,self.eng,self.math,self.total,self.avg,self.rank]
 
     def __str__(self):
-        return f"name:{self.name}  kor:{self.kor} eng:{self.eng} math:{self.math} total:{self.total}  avg:{self.avg}"
+        return f"name:{self.name}  kor:{self.kor} eng:{self.eng} math:{self.math} total:{self.total}  avg:{self.avg} rank:{self.rank}"
 
 class StudentsMsg:
     def __init__(self, studentsList):  # 학생객체 리스트를 갖는다
@@ -41,10 +43,57 @@ class StudentsMsg:
     def displayRank(self):  # 성적순으로 학생 정보 출력
         pass
 
-s = Students()
-s.inputData()
-print(s)
+# 학생객체를 저장
+students = []
+for i in range(2):
+    students.append(Students().inputData())
 
-# a = StudentsMsg()
-# a.inputData()
-# StudentsMsg().inputData()
+#학생객체를 저장하고 있는 리스트중에 학생의 totla값을 기준으로 정렬
+students.sort(key=lambda obj:obj.total, reverse=True)
+# 객체의 맴버변수들의 값을 확인
+# for i in students:
+#     print(i)
+
+#석차 셋팅
+totalList = [i.total for i in students]
+for idx,value in enumerate(totalList):
+    students[idx].rank =  totalList.index(value)+1
+
+# 학생객체의 정보를 다 출력해서 rank가 제대로 셋팅되었는지 확인
+# rank순으로 정렬
+students.sort(key =lambda obj : obj.rank)
+for i in students:
+    print(i)
+
+# 숙제
+# 1등학생의  이름은?
+
+
+# #
+# import random as rd
+# scores = [58, 90, 76, 72, 60, 60, 41, 63, 96, 83, 47, 64, 82, 75, 45, 78, 79, 82, 48, 97] #rd.sample(range(40,100+1),20)
+# print(scores)
+# scores.sort(reverse=True)
+# print(scores)
+# rank = []
+# for i in scores:
+#     rank.append(scores.index(i)+1)
+# print(rank)
+#
+#
+# temp = [
+#     ['a',70,20,30],
+#     ['b',20,30,40],
+#     ['c',50,10,30]
+# ]
+# from operator import itemgetter
+# temp.sort(key=itemgetter(1))
+# print(temp)
+#
+# temp2 = [
+#     {'name':'a','kor':70, 'eng':20, 'math':30},
+#     {'name':'b','kor':20, 'eng':30, 'math':40},
+#     {'name':'c','kor':20, 'eng':10, 'math':30}
+# ]
+# temp2.sort(key=lambda a : (a['kor'], a['eng'] ) )
+# print(temp2)
